b88ce81dd54215d366ca82fa9dbf3ec9f39a691d
Josh Hoffman <Josh.Hoffman@rga.com>
2013-03-06 11:59:27 -0500

    no message

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php 
+	error_reporting(E_ALL);
+ ini_set("display_errors", 1);
+
 	include_once('service/mongoControl.php');
 	$md = new mongoDBcontrol('pluggedIn');
 	$results = $md->find('bandList');
