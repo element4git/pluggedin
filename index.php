f802855f673065d8c668ec1003a121e2f6b7e832
Josh Hoffman <Josh.Hoffman@rga.com>
2013-03-11 16:51:44 -0400

    test

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 			$FBID = '439511386129295';
 			$soundCloudID = '37b4cbf041d27eafb17741805c38ceda';
 			$soundCloudRedirect = 'http://pluggedin.rga.com/soundcloudAuth/';
-			define('mongoServer','mongodb://dbuser:fj47FH47hfh@ds041167.mongolab.com:41167/pluggedin');
+			define('mongoServer','mongodb://dbuser:fj47FH47hfh@ds041167.mogolab.com:41167/pluggedin');
 		break;
 		default:
 			$FBID = '456738747726141';
