b97ecddfc8f75c6680da8ec0dc88828ab3d0d213
Josh Hoffman <Josh.Hoffman@rga.com>
2013-03-12 12:39:56 -0400

    updated error page

--- index.php
+++ index.php
@@ -25,7 +25,23 @@
 	try{
 		$md = new mongoDBcontrol('pluggedIn'); }
 	catch (Exception $e){
-		echo "There seems to be a problem with the server. Please try again, if this problem persists try again in a few minutes. Sorry for the trouble.";
+		echo '		
+			<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+			<html xmlns="http://www.w3.org/1999/xhtml">
+			<head>
+			<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+			<title>Please try again</title>
+			<style>
+				body{
+					background-color:#000;	
+				}
+			</style>
+			</head>
+			
+			<body>
+				<div style="margin:0 auto; width:640px;"><img src="images/plugged_in_error.jpg" /></div>
+			</body>
+			</html>';
 		return false;
 	}
 	
