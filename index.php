cacc93f46de03e43cbac85ee497046df2dbcef5f
ericfino <eric.fino@rga.com>
2013-03-13 18:19:56 -0400

    - various updates to visual layout esp for results list. calendar icon update. updated logo

--- index.php
+++ index.php
@@ -1,90 +1,4 @@
-<?php 
-	error_reporting(E_ALL);
- 	ini_set("display_errors", 1);
-	$server = explode('www.',$_SERVER['SERVER_NAME']);
-	
-	$server = (isset($server[1])) ?  $server[1] : $server[0];
-	
-	switch($server){
-		case 'pluggedin.rga.com':
-			$FBID = '439511386129295';
-			$soundCloudID = '37b4cbf041d27eafb17741805c38ceda';
-			$soundCloudRedirect = 'http://pluggedin.rga.com/soundcloudAuth/';
-			define('mongoServer','mongodb://dbuser:fj47FH47hfh@ds053607.mongolab.com:53607/pluggedin');
-		break;
-		default:
-			$FBID = '456738747726141';
-			$soundCloudID = '303569302e749627d95c37b8b1666cbb';
-			$soundCloudRedirect = 'http://pluggedin.com/soundcloudAuth';
-			define('mongoServer', 'mongodb://127.0.0.1');
-		break;
-	}
 
-	include_once('service/mongoControl.php');
-	
-	try{
-		$md = new mongoDBcontrol('pluggedIn'); }
-	catch (Exception $e){
-		echo '		
-			<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-			<html xmlns="http://www.w3.org/1999/xhtml">
-			<head>
-			<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-			<title>Please try again</title>
-			<style>
-				body{
-					background-color:#000;	
-				}
-			</style>
-			</head>
-			
-			<body>
-				<div style="margin:0 auto; width:640px;"><img src="images/plugged_in_error.jpg" /></div>
-			</body>
-			</html>';
-		return false;
-	}
-	
-	
-	$results = $md->find('bandList');
-	$results = iterator_to_array($results);
-		
-	$patern = "/[-!$%^&*()_+|~=`{}\[\]:\";'<>?,.\/]|@| /";
-	foreach($results as $key => $value){
-		$gigs = $results[$key]['response']['gigs'];
-	}
-	$html = '';
-	$searchArray = array();
-	foreach($gigs as $key => $gig){
-		$cleanBandName = preg_replace($patern,'',$gig['band_name']);
-		$cleanVenueName = preg_replace($patern,'',$gig['venue_name']);
-		
-		$searchArray[] = $gig['band_name'];
-		$searchArray[] = $gig['venue_name'];
-		
-		$html .= '<div class="full-width gig '.$cleanBandName.' '.$cleanVenueName.'"><div id="eventTime" class="event-time grid-3">'.$gig['start_time'].'</div><div class="grid-7"><div id="bandName" class="band-name full-width">'.$gig['band_name'].'</div><div id="venueName" class="venue-name full-width"><a>'.$gig['venue_name'].'</a></div></div><div class="add-to-cal grid-2"><a class="ico-calendar"></a></div><div class="gigInfo"><input type="hidden" name="date" value="'.$gig['date'].'" /></div></div>';
-	}
-	
-	$html = str_replace("'","\\'",$html);
-	
-	$html =  '$(\'<div id="gightml">'.$html.'</div>\')';
-	
-	//print_r($html); return false;
-	
-	$searchArray = array_unique($searchArray);
-	
-	$searchArray = array_values($searchArray);
-	
-	
-	/*?>
-	gigs = sxswMusic[n].response.gigs;
-	
-	var pattern = /[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]|@| /g,
-				cleanBandName = gig.band_name.replace(pattern,''),
-				cleanVenueName = gig.venue_name.replace(pattern,'');
-				
-				html = '<div class="full-width gig '+cleanBandName+' '+cleanVenueName+'"><div id="eventTime" class="event-time grid-3">'+gig.start_time+'</div><div class="grid-7"><div id="bandName" class="band-name full-width">'+gig.band_name+'</div><div id="venueName" class="venue-name full-width"><a>'+gig.venue_name+'</a></div></div><div class="add-to-cal grid-2"><a class="ico-calendar"></a></div><div class="gigInfo"><input type="hidden" name="date" value="'+gig.date+'" /></div></div>'<?php */
-?>
 
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html xmlns:fb="http://www.facebook.com/2008/fbml">
@@ -102,12 +16,12 @@
 	<div id="mainContainer" class="full-width">
         <div id="mainContent" class="full-width">
             <div id="topSpacer" class="grid-container">
-                <div class="grid-12 pad-8">&nbsp;</div>
+                <div class="grid-12"></div>
             </div>
             <div id="filters" class="full-width">
                 <div class="grid-container">
                     <div class="grid-12">
-                    	<h1 class="connect-lbl">Connect to local bands you like playing nearby</h1>
+                    	<h1 class="connect-lbl">Connect to locate bands you like playing nearby</h1>
                     </div>
                 </div>
                 <div class="social-btns grid-container">
@@ -129,9 +43,9 @@
                 	</div>
                 </div>
                 <div class="divider grid-container">
-                    <div class="grid-5"><hr/></div>
+                    <div class="grid-5"><hr class="pad-left"/></div>
                     <div class="grid-2"><h2>or</h2></div>
-                    <div class="grid-5"><hr/></div>
+                    <div class="grid-5"><hr class="pad-right"/></div>
                 </div>
                 <div class="search-bar grid-container">
                     <div class="grid-12">
@@ -141,38 +55,58 @@
 						</form>
 					</div>
                 </div>
-   				<div id="masterList" class="full-width"> <!-- preferred name: schedule -->
-                <div class="grid-container">
-                   <!-- <div id="eventDate" class="event-date grid-12">Today</div>
-                    <div class="full-width">
-                        <div id="eventTime" class="event-time grid-3">7:30pm</div>
-                        <div class="grid-7">
-                        	<div id="bandName" class="band-name full-width">The Greatest Band in the World</div>
-                        	<div id="venueName" class="venue-name full-width"><a>Lounge #1</a></div>
-                        </div>
-                        <div class="add-to-cal grid-2"><a class="ico-calendar"></a></div>
-                    </div>-->
-                </div>
-                <!--<div class="grid-container">
-                    <div id="eventDate" class="event-date grid-12">3/12/2013</div>
-                    <div class="full-width">
-                        <div id="eventTime" class="event-time grid-3">8:00pm</div>
-                        <div class="grid-7">
-                        	<div id="bandName" class="band-name full-width">Bano</div>
-                        	<div id="venueName" class="venue-name full-width"><a>Dive Bar</a></div>
-                        </div>
-                        <div class="add-to-cal grid-2"><a class="ico-calendar"></a></div>
-                    </div>
-                    <div class="full-width">
-                        <div id="eventTime" class="event-time grid-3">9:15pm</div>
-                        <div class="grid-7">
-                        	<div id="bandName" class="band-name full-width">The 2nd Greatest Band in the World</div>
-                        	<div id="venueName" class="venue-name full-width"><a>Stage 6</a></div>
-                        </div>
-                        <div class="add-to-cal grid-2"><a class="ico-calendar"></a></div>
-                    </div>
-                </div>-->
             </div>
+            <div id="masterList" class="full-width"> <!-- preferred name: schedule -->
+	            <div class="grid-container-pad0">
+	               <div id="eventDate" class="event-date full-width"><span>Today</span></div>
+	                <div class="cal-container">
+	                    <div id="eventTime" class="event-time grid-3">8:88pm</div>
+	                    <div class="grid-7">
+	                    	<div id="bandName" class="band-name full-width">Duologue</div>
+	                    	<div id="venueName" class="venue-name full-width"><a>Creekside at Hilton Garden Inn</a></div>
+	                    </div>
+	                    <div class="add-to-cal grid-2"><a class="ico-calendar"></a></div>
+	                </div>
+	                <div class="cal-container">
+	                    <div id="eventTime" class="event-time grid-3">9:15pm</div>
+	                    <div class="grid-7">
+	                    	<div id="bandName" class="band-name full-width">The 2nd Greatest Band in the World</div>
+	                    	<div id="venueName" class="venue-name full-width"><a>Stage 6</a></div>
+	                    </div>
+	                    <div class="add-to-cal grid-2"><a class="ico-calendar"></a></div>
+	                </div>
+	            </div>
+	            <div class="grid-container-pad0">
+	                <div id="eventDate" class="event-date full-width"><span>3/12/2013</span></div>
+	                <div class="cal-container">
+	                    <div id="eventTime" class="event-time grid-3">8:00pm</div>
+	                    <div class="grid-7">
+	                    	<div id="bandName" class="band-name full-width">Bano</div>
+	                    	<div id="venueName" class="venue-name full-width"><a>Dive Bar</a></div>
+	                    </div>
+	                    <div class="add-to-cal grid-2"><a class="ico-calendar"></a></div>
+	                </div>
+	            </div>
+	            <div class="grid-container-pad0">
+	                <div id="eventDate" class="event-date full-width"><span>Tomorrow</span></div>
+	                <div class="cal-container">
+	                    <div id="eventTime" class="event-time grid-3">8:00pm</div>
+	                    <div class="grid-7">
+	                    	<div id="bandName" class="band-name full-width">Bano</div>
+	                    	<div id="venueName" class="venue-name full-width"><a>Dive Bar</a></div>
+	                    </div>
+	                    <div class="add-to-cal grid-2"><a class="ico-calendar"></a></div>
+	                </div>
+	                <div class="cal-container">
+	                    <div id="eventTime" class="event-time grid-3">9:15pm</div>
+	                    <div class="grid-7">
+	                    	<div id="bandName" class="band-name full-width">The 2nd Greatest Band in the World</div>
+	                    	<div id="venueName" class="venue-name full-width"><a>Stage 6</a></div>
+	                    </div>
+	                    <div class="add-to-cal grid-2"><a class="ico-calendar"></a></div>
+	                </div>
+	            </div>
+	    	</div>
 		</div>
 		<div id="footer" class="full-width">
 	        <div class="wrapper">
